ab87ccc310e91f3aed3053c26926152726e203a9
LiTugou <45063124+LiTugou@users.noreply.github.com>
2022-10-18 15:21:47 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ tag=${latest_tag##*/}
 git clone -b ${tag} https://github.com/troyeguo/koodo-reader.git
 rm koodo-reader/.git -r
 cd koodo-reader
-npx browserslist@latest --update-db
 yarn && yarn build
 tar -czf koodo-reader-build.tar.gz -C ./build .
 cd ../
