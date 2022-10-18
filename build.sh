d486d21c61bffcf70121ea7769733fba0af907c2
LiTugou <45063124+LiTugou@users.noreply.github.com>
2022-10-18 17:01:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,12 @@ tag=${latest_tag##*/}
 git clone -b ${tag} https://github.com/troyeguo/koodo-reader.git
 rm koodo-reader/.git -r
 cd koodo-reader
-echo 'PUBLIC_URL="https://cdn.jsdelivr.net/gh/LiTugou/koodo-reader@main"' > .env
+# echo 'PUBLIC_URL="https://cdn.jsdelivr.net/gh/LiTugou/koodo-reader@main"' > .env
+PUBLIC_URL="https://cdn.jsdelivr.net/gh/LiTugou/koodo-reader@main"
+echo "${PUBLIC_URL}" > .env
+#sed -i "s?\./assets?${PUBLIC_URL}/assets?g" launchUtil.tsx
 yarn && yarn build
+sed -i "s?\./assets?${PUBLIC_URL}/assets?g" build/static/js/main.*.js
 tar -czf koodo-reader-build.tar.gz -C ./build .
 cd ../
 shopt -s extglob
